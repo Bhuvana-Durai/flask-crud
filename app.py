5f053c12b08052c5c1d24f915d6367252546a88e
Bhuvana-Durai <bhuvanadtech@gmail.com>
2025-01-04 19:47:03 +0530

    feat: implement user insertion functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for,request,redirect
 from flask_mysqldb import MySQL
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 mysql = MySQL(app)
 
 
-#creation of data
+#homepage loading
 @app.route("/")
 def home():
     con = mysql.connection.cursor()
@@ -19,5 +19,23 @@ def home():
     con.execute(sql)
     res = con.fetchall()
     return render_template("home.html",datas = res)
+
+#user update
+@app.route("/addusers",methods=['GET','POST'])
+def addusers():
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        city = request.form['city']
+        con = mysql.connection.cursor()
+        sql = "insert into users(NAME,AGE,CITY) values (%s,%s,%s)"
+        con.execute(sql,[name,age,city])
+        mysql.connection.commit()
+        con.close()
+        return redirect(url_for("home"))
+    return render_template("addusers.html")
+
+
+
 if(__name__ == '__main__'):
     app.run(debug=True)
