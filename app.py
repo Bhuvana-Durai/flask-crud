e23b217de8b35de2c9ac574d07939fe1ebac8f6e
Bhuvana-Durai <bhuvanadtech@gmail.com>
2025-01-05 11:32:20 +0530

    feat: delete operation implemented

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for,request,redirect
+from flask import Flask,render_template,url_for,request,redirect,flash
 from flask_mysqldb import MySQL
 app = Flask(__name__)
 
@@ -32,10 +32,43 @@ def addusers():
         con.execute(sql,[name,age,city])
         mysql.connection.commit()
         con.close()
+        flash("User details added..")
         return redirect(url_for("home"))
     return render_template("addusers.html")
 
 
+#update users
+@app.route("/editusers/<string:id>",methods = ['GET','POST'])
+def editusers(id):
+    con = mysql.connection.cursor()
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        city = request.form['city']
+        con = mysql.connection.cursor()
+        sql = "update users set NAME= %s, AGE = %s, CITY = %s where ID = %s"
+        con.execute(sql,[name,age,city,id])
+        mysql.connection.commit()
+        con.close()
+        flash("User detail updated..")
+        return redirect(url_for("home"))
+    sql = "select * from users where ID=%s"
+    con.execute(sql,[id])
+    res = con.fetchone()
+    return render_template("edituser.html",datas = res)
+
+
+#delete user
+@app.route("/deleteuser/<string:id>",methods = ['GET','POST'])
+def deleteuser(id):
+    con = mysql.connection.cursor()
+    sql = "delete from users where id=%s"
+    con.execute(sql,[id])
+    mysql.connection.commit()
+    con.close()
+    flash("User detail deleted..")
+    return redirect(url_for("home"))
 
 if(__name__ == '__main__'):
+    app.secret_key = "abc123"
     app.run(debug=True)
